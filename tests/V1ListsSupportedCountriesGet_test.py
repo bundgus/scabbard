693742b6d5a76926e7873545849aceb1544d32d5
bundgus <bundgus@gmail.com>
2017-12-27 12:23:23 -0600

    initial commit

---
+++ tests/V1ListsSupportedCountriesGet_test.py
@@ -0,0 +1,21 @@
+from scabbard import get_client
+
+client = get_client()
+
+countries = client.Air_Utility.V1ListsSupportedCountriesGet(pointofsalecountry='NZ').result()
+
+print('PointOfSale')
+print(countries.PointOfSale)
+
+print('OriginCountries')
+for c in countries.OriginCountries:
+    print(c.CountryCode, c.CountryName)
+
+print('DestinationCountries')
+for c in countries.DestinationCountries:
+    print(c.CountryCode, c.CountryName)
+
+print('Links')
+for l in countries.Links:
+    print(l.rel)
+    print(l.href)
