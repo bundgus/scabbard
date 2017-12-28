2248644f69679881a2109b937097c83c2e862da4
bundgus <bundgus@gmail.com>
2017-12-28 15:07:05 -0600

    initial commit

--- scabbard/scabbard.py
+++ scabbard/scabbard.py
@@ -2,6 +2,7 @@ from bravado.requests_client import RequestsClient
 from bravado.client import SwaggerClient
 from bravado.swagger_model import load_file
 from scabbard.access_token import get_token
+import pkg_resources
 
 
 def get_client():
@@ -11,5 +12,11 @@ def get_client():
         'api.test.sabre.com', access_token,
         param_name='Authorization', param_in='header'
     )
-    return SwaggerClient.from_spec(load_file('scabbard/swagger.yaml'),
+
+    resource_package = __name__
+    resource_path = '/swagger.yaml'  # Do not use os.path.join()
+
+    swagger_path = pkg_resources.resource_filename(resource_package, resource_path)
+    return SwaggerClient.from_spec(load_file(swagger_path),
                                    http_client=http_client)
+
