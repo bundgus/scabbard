d1277f0c176c08a9fc31df372194dbc6286d1017
bundgus <bundgus@gmail.com>
2017-12-30 14:30:53 -0600

    initial commit

--- tests/Air_Intelligence/test_V1HistoricalFlightsDestinationSeasonalityGet.py
+++ tests/Air_Intelligence/test_V1HistoricalFlightsDestinationSeasonalityGet.py
@@ -5,9 +5,6 @@ from datetime import datetime, timedelta
 def test__v1_historical_flights_destination_seasonality_get():
     client = get_client()
 
-    departuredate = (datetime.now() + timedelta(days=7)).strftime('%Y-%m-%d')
-    returndate = (datetime.now() + timedelta(days=14)).strftime('%Y-%m-%d')
-
     seasonality = client.Air_Intelligence\
         .V1HistoricalFlightsDestinationSeasonalityGet(destination='DFW')\
         .result()
