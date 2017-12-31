bdea0c9c1f46ca1b6da99d02f16f040b0f9d6c10
bundgus <bundgus@gmail.com>
2017-12-31 08:15:17 -0600

    initial commit

--- tests/Air_Search/test_V2ShopFlightsFaresGet.py
+++ tests/Air_Search/test_V2ShopFlightsFaresGet.py
@@ -1,13 +1,12 @@
 from scabbard import get_client
 
 
-def test_v2_shop_flights_fares_get():
+def test_v2_shop_flights_fares_get_lead_price_calendar():
     client = get_client()
 
     fares = client.Air_Search\
         .V2ShopFlightsFaresGet(origin='JFK',
-                               destination='LAX',
-                               lengthofstay='5',
+                               lengthofstay=5,
                                pointofsalecountry='US'
                                )\
         .result()
@@ -21,3 +20,16 @@ def test_v2_shop_flights_fares_get():
     #        print(c['LowestNonStopFare']['Fare'])
     #    elif isinstance(c['LowestNonStopFare'], str):  # if value is 'N/A'
     #        print(c['LowestNonStopFare'])
+
+
+def test_v2_shop_flights_fares_get_destination_finder():
+    client = get_client()
+
+    fares = client.Air_Search\
+        .V2ShopFlightsFaresGet(origin='JFK',
+                               destination='LAX',
+                               lengthofstay=5,
+                               pointofsalecountry='US'
+                               )\
+        .result()
+    assert 1 <= len(fares['FareInfo'][0]['CurrencyCode'])
